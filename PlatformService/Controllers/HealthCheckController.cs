33fae38414ccecbba8c9a78e4472cb60bf9e8e15
gabrielkf <gabrielkf@gmail.com>
2023-01-08 19:40:58 -0300

    Project setup

---
+++ PlatformService/Controllers/HealthCheckController.cs
@@ -0,0 +1,12 @@
+﻿using Microsoft.AspNetCore.Mvc;
+
+namespace PlatformService.Controllers
+{
+    [ApiController]
+    [Route("[controller]")]
+    public class HealthCheckController : ControllerBase
+    {
+        [HttpGet]
+        public string Get() => "Platform Service up and running";
+    }
+}
